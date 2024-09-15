545466663b424f59777b8f3f2e2ea4fa7eda4afb
lxdua <2753792469@QQ.COM>
2024-09-15 12:24:54 +0800

    压力板补刀

--- rgj/Script/button.gd
+++ rgj/Script/button.gd
@@ -1,14 +1,29 @@
 extends Area2D
 
 
-var is_on: bool:
-	get:
-		return not bodies.is_empty()
+var is_on: bool = false:
+	set(v):
+		if is_on == v:
+			return
+		is_on = v
+		for i in control:
+			i.update(is_on)
+
+
+@export var control: Array[Node2D]
 
 var bodies: Array[Node2D]
 
 func _on_body_entered(body: Node2D) -> void:
 	bodies.append(body)
+	if bodies.is_empty():
+		is_on = false
+	else:
+		is_on = true
 
 func _on_body_exited(body: Node2D) -> void:
 	bodies.erase(body)
+	if bodies.is_empty():
+		is_on = false
+	else:
+		is_on = true
