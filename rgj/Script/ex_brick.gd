f6d646c704c40be020b1c2a6ff9203a2732b601a
lxdua <2753792469@QQ.COM>
2024-09-16 12:27:02 +0800

    删掉倒计时

--- rgj/Script/ex_brick.gd
+++ rgj/Script/ex_brick.gd
@@ -1,7 +1,6 @@
 extends StaticBody2D
 
 @onready var time_label: Label = $TimeLabel
-@onready var timer: Timer = $Timer
 
 var is_on: bool = false:
 	set(v):
@@ -10,13 +9,5 @@ var is_on: bool = false:
 		is_on = v
 		for i in control:
 			i.update(is_on)
-		if is_on:
-			timer.start()
 
 @export var control: Array[Node2D]
-
-func _physics_process(delta: float) -> void:
-	time_label.text = str(int(timer.time_left))
-
-func _on_timer_timeout() -> void:
-	is_on = false
