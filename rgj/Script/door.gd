3ee1e8be3a816ab587bc6bd0f427f392a7ebff74
lxdua <2753792469@QQ.COM>
2024-09-16 10:07:55 +0800

    补刀钥匙和门，还没测试

--- rgj/Script/door.gd
+++ rgj/Script/door.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
+@export var next_level: PackedScene
 
-
+var can_go := false
+var player_num := 0
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	player_num+=1
+	if can_go and player_num==2:
+		get_tree().change_scene_to_packed(next_level)
+
+func _on_body_exited(body: Node2D) -> void:
+	player_num-=1
--- rgj/Script/key.gd
+++ rgj/Script/key.gd
@@ -1,1 +1,5 @@
 extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	get_tree().get_first_node_in_group("Door").can_go = true
